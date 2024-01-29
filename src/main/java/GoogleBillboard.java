0ea6f412a1a741722a22b5951947c585022570b3
ailew <143014764+ailew@users.noreply.github.com>
2024-01-28 23:11:02 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,24 +1,38 @@
 public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 public void setup()  
-{ 
-  for (int i = 12; i <= e.length() - 10; i++) {
-    double dNum = Double.parseDouble(e.substring(i - 10, i));
-    System.out.println(dNum);
-    if (isPrime(dNum)) {
-      break;
+{            
+    boolean found = false;
+    int counter = 0;
+    double tenDigits = 0;
+    while(!found){
+      tenDigits = Double.parseDouble(e.substring(counter, counter + 10));
+      if(isPrime(tenDigits)){
+        found = true;
+      } else {
+        counter++;
+      }
     }
-  }
+    System.out.println(tenDigits);
 }  
 public void draw()  
 {   
   //not needed for this assignment
 }  
-public boolean isPrime(double dNum)  
-{   
-  for (int i = 2; i <= Math.sqrt(dNum); i++) {
-    if (dNum % i == 0) {
+public boolean isPrime(double num){
+  if(num == 1 || num < 0){
+    return false;
+  }
+  if(num % 1 != 0){
+    return false;
+  }
+  if(num == 2){
+    return true;
+  }
+  int sum = (int)Math.sqrt(num);
+  for(int i = 2; i <= sum; i++){
+    if(num % i == 0){
       return false;
     }
   }
   return true;
-} 
+}
