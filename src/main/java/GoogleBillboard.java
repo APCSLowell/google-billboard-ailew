2d8988987bfe0144bd3b290c1d4a36122a5bf437
ailew <143014764+ailew@users.noreply.github.com>
2024-01-28 23:17:38 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,17 +1,14 @@
-public class Main {
-    public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435"; 
-  static int a = 2;
-  static int b = 12;
-
-    public static void main(String[] args) {
-        for(int i = 10; i < ee.length(); i++){
-            if(isPrime(Double.parseDouble(ee.substring(i-10, i)))){
-             System.out.println(ee.substring(i-10, i));
-                break;
-            }
-         }
-    
-    }
+public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+public final static String ee = "27182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";
+public void setup()  
+{            
+ for(int i = 10; i < ee.length(); i++){
+   if(isPrime(Double.parseDouble(ee.substring(i-10, i)))){
+     System.out.println(ee.substring(i-10, i));
+     break;
+   }
+ }
+}  
     
   public static boolean isPrime(double num) { 
     if(num <2){
