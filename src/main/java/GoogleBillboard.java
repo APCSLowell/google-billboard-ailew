5536a69e6915c5a93c547fd6c3c63d37672df00e
ailew <143014764+ailew@users.noreply.github.com>
2024-01-28 23:24:33 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,25 +1,27 @@
-public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-public final static String ee = "27182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";
-public void setup()  
-{            
- for(int i = 10; i < ee.length(); i++){
-   if(isPrime(Double.parseDouble(ee.substring(i-10, i)))){
-     System.out.println(ee.substring(i-10, i));
-     break;
-   }
- }
-}  
-    
-  public boolean isPrime(double num) { 
-    if(num <2){
-        return false;
-    }
-    for (int i = 2; i<=Math.sqrt(num);i=i+1){
-      if (num%i==0){
-        return false;
-      }
+public class Main {
+    public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= e.length() - 10; i++) {
+            String digits = e.substring(i, i + 10);
+            double dNum = Double.parseDouble(digits);
+
+            if (isPrime((long) dNum)) {
+                System.out.println("First 10-digit prime in 'e': " + digits);
+                break;
+            }
+        }
     }
 
-    return true;
-  } 
+    public static boolean isPrime(long num) {
+        if (num < 2) {
+            return false;
+        }
+        for (long i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
