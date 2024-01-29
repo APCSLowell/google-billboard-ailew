044d77b0d1523e28f11c93dc3ed478db07a1d2a4
ailew <143014764+ailew@users.noreply.github.com>
2024-01-28 22:50:11 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -9,6 +9,17 @@ public class GoogleBillboard {
 
 	//Finish this function
 	public boolean isPrime(double d){
+  		if(num < 2){
+    			return false;
+  		}
+ 		for(int i = 2; i<=Math.sqrt(num); i++){
+    			if(num % i == 0){
+      				return false;
+    			}
+  		}
+  		return true;
+  
+
 	}
 
 }
