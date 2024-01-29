aa954776ea074b9ab62ca5ca75dcce6b6b3ae9d7
ailew <143014764+ailew@users.noreply.github.com>
2024-01-28 23:09:22 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,32 +1,24 @@
-public class Main {
-    public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435"; 
-  static int a = 2;
-  static int b = 12;
-
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-      for (int g = 0; g<=99; g=g+1){
-        a++;
-        b++;
-        String digits = e.substring(a,b);
-        double output = Double.parseDouble(digits);
-        if (isPrime(output)==true){
-          System.out.println(output);
-        }
-      }
-    
+public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+public void setup()  
+{ 
+  for (int i = 12; i <= e.length() - 10; i++) {
+    double dNum = Double.parseDouble(e.substring(i - 10, i));
+    System.out.println(dNum);
+    if (isPrime(dNum)) {
+      break;
     }
-    
-  public static boolean isPrime(double num) { 
-    if(num <2){
-        return false;
+  }
+}  
+public void draw()  
+{   
+  //not needed for this assignment
+}  
+public boolean isPrime(double dNum)  
+{   
+  for (int i = 2; i <= Math.sqrt(dNum); i++) {
+    if (dNum % i == 0) {
+      return false;
     }
-    for (int g = 2; g<=Math.sqrt(num);g=g+1){
-      if (num%g==0){
-        return false;
-      }
-    }
-
-    return true;
-  } 
-}
+  }
+  return true;
+} 
